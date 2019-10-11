3ff696e5f746c4e9f89c48840a45e80dff71ad72
Ron-Amado <56396405+Ron-Amado@users.noreply.github.com>
2019-10-11 14:27:11 +0300

    Add files via upload

---
+++ ronp2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 23:09:24 2019
+
+@author: ADMIN
+"""
+#Name: Ron Amado
+#Class: YA-4 ׁׂ(יא-4)
+
+
+def MakeDict():
+    dict1={}
+    for x in range(5):
+        dict1[x]= x**2
+    return dict1
+
+        
+dict = MakeDict()
+print (dict) 
